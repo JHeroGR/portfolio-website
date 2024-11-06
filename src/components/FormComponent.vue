6f80f2fdf6775a8dd5bd89405aa7cd33aa532a0f
JHero <jheroytc@gmail.com>
2024-11-06 14:38:12 -0600

    added social media icons Github and Instagram to AboutView and made FormComponent separate from ContactView - AboutView

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,37 +1,62 @@
 <template lang="">
-    <div class='container'>
-        <form>
-            <div class='container justify-content-lg-center'>
-                <div class='row'>
-                    <div class='col'>
-                        <input type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Name / Agency / Company">
-                    </div>
-                    <div class='col'>
-                        <input type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Email">
-                    </div>
-                </div>
-                <div class='row'>
-                    <div class='col'>
-                        <input type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Phone Number">
-                    </div>
-                    <div class='col'>
-                        <input type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Subject">
-                    </div>
-                </div>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder='Message'></textarea>
-                <br>
-                <input type='submit' class='form-control btn btn-success'>
+  <div class="container">
+    <form>
+      <div class="bg-white border rounded shadow-sm overflow-hidden">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <input
+                type="text"
+                class="form-control mb-2"
+                id="inlineFormInput"
+                placeholder="Name / Agency / Company"
+              />
             </div>
-        </form>
-    </div>
+            <div class="col">
+              <input
+                type="text"
+                class="form-control mb-2"
+                id="inlineFormInput"
+                placeholder="Email"
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <input
+                type="text"
+                class="form-control mb-2"
+                id="inlineFormInput"
+                placeholder="Phone Number"
+              />
+            </div>
+            <div class="col">
+              <input
+                type="text"
+                class="form-control mb-2"
+                id="inlineFormInput"
+                placeholder="Subject"
+              />
+            </div>
+          </div>
+          <textarea
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+            placeholder="Message"
+          ></textarea>
+          <br />
+          <input type="submit" class="form-control btn btn-success" />
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'FormComponent'
-}
+    name: "FormComponent",
+};
 </script>
 
-<style lang="">
-
-</style>
+<style></style>
