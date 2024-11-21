37f5da8dc06916716f03b0b38164109d469a5cf3
JHero <jheroytc@gmail.com>
2024-11-20 23:50:59 -0600

    configured publicPath for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  base: "/portfolio-website/",
-  plugins: [],
-  resolve: {
-    alias: [
-      
-    ]
-  },
-})
+  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+});
