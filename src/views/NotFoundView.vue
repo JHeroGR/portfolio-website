a4dacb537cc5b29fd717438695ba5dc99f37b8f1
Joel Gutierrez <jheroytc@gmail.com>
2025-04-14 22:49:23 -0500

    added a timer to return back to main page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,9 +1,15 @@
 <template>
     <h2>404 NOT FOUND</h2>   
+    <h3>Redirecting back to home...</h3>
 </template>
 
 <script>
 export default {
     name: "NotFoundView",
+    mounted() {
+    setTimeout(() => {
+      this.$router.push('/');
+    }, 2000); // redirect after 2s
+  }
 }
 </script>
